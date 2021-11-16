09a5148cf94523f110789e07fe9f0b1b1ad88847
IPECTER <80433772+IPECTER@users.noreply.github.com>
2021-11-16 17:30:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+![Banner](https://imgur.com/yqujPbM.png)
+#### Illuminate the night with breathtaking ambient particle effects
+
+## Overview
+Aurora provides a unique experience for all players by generating dynamic **ambient particle effects** using a **high-performance** particle engine. All particles are **fully configurable** and can be **customised per-biome** to fit each server's needs. The base plugin comes with multiple **high-quality default configs** covering all particle shapes Aurora has to offer, available for almost all vanilla biomes.
+
+- [LightAPI](https://www.spigotmc.org/resources/lightapi.4510/)
+
+## Wiki
+#### Aurora
+- [Home](https://github.com/OpticFusion1/Aurora/wiki)
+- [Features](https://github.com/OpticFusion1/Aurora/wiki/Features)
+- [Commands](https://github.com/OpticFusion1/Aurora/wiki/Commands)
+- [Permissions](https://github.com/OpticFusion1/Aurora/wiki/Permissions)
+
+#### Configuration
+- [General Configuration](https://github.com/OpticFusion1/Aurora/wiki/General-Configuration)
+- [Particle Configuration](https://github.com/OpticFusion1/Aurora/wiki/Particle-Configuration)
+- [Biome Presents](https://github.com/OpticFusion1/Aurora/wiki/Biome-Presents)
+
+#### Support
+- [Frequently Asked Questions](https://github.com/OpticFusion1/Aurora/wiki/Frequently-Asked-Questions)
+- [WorldGuard Compatibility](https://github.com/OpticFusion1/Aurora/wiki/WorldGuard-Compatibility)
+- [Developer API](https://github.com/OpticFusion1/Aurora/wiki/Developer-API)
+- [Terms Of Service](https://github.com/OpticFusion1/Aurora/wiki/Terms-Of-Service)
+
+## More Info
+[Download](https://www.spigotmc.org/resources/%E2%98%84%EF%B8%8Faurora%E2%98%84%EF%B8%8F-ambient-particle-display-customisable-per-biome.89399/) (SpigotMC)<br>
+[Support](https://discord.gg/jMEsq4zHVA) (Discord)
---
+++ src/main/java/com/zenya/aurora/util/ext/LightAPI.java
@@ -0,0 +1,57 @@
+package com.zenya.aurora.util.ext;
+
+import org.bukkit.Location;
+import ru.beykerykt.minecraft.lightapi.common.api.engine.EditPolicy;
+import ru.beykerykt.minecraft.lightapi.common.api.engine.SendPolicy;
+
+/**
+ * LightAPI https://github.com/BeYkeRYkt/LightAPI
+ *
+ * @version 5.0.0-preview
+ *
+ */
+public class LightAPI {
+
+  public static LightAPI INSTANCE = new LightAPI();
+  private static ru.beykerykt.minecraft.lightapi.common.LightAPI mAPI;
+
+  public LightAPI() {
+    mAPI = ru.beykerykt.minecraft.lightapi.common.LightAPI.get();
+  }
+
+  public static void disable() {
+  }
+
+  /**
+   * A method to remove lighting and instantly update it client-side
+   *
+   * @param location Location to remove lighting from
+   * @param lightFlags Type of lighting to remove
+   * @param async Whether light updating should be asynchronous
+   * @param force Enable force mode
+   * @see #setLight(Location, int, int, boolean, boolean)
+   */
+  public static void clearLight(Location location, int lightFlags, boolean async, boolean force) {
+    setLight(location, lightFlags, 0, async, force);
+  }
+
+  /**
+   * A method to set lighting and instantly update it client-side
+   *
+   * @param location Location to create lighting
+   * @param lightLevel Light level to set
+   * @param lightFlags Type of lighting to create
+   * @param async Whether light updating should be asynchronous
+   * @param force Enable force mode
+   */
+  public static void setLight(Location location, int lightFlags, int lightLevel, boolean async, boolean force) {
+    EditPolicy editPolicy = async ? EditPolicy.DEFERRED : EditPolicy.IMMEDIATE;
+    SendPolicy sendPolicy = async ? SendPolicy.DEFERRED : SendPolicy.IMMEDIATE;
+    if (force) {
+      editPolicy = EditPolicy.FORCE_IMMEDIATE;
+      sendPolicy = SendPolicy.IMMEDIATE;
+    }
+    mAPI.setLightLevel(location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ(),
+            lightLevel, lightFlags, editPolicy, sendPolicy, null);
+  }
+}
